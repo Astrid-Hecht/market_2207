0775fbbe08dab15eb05eb57f812d7d38bf4c2fdb
Astrid Hecht <silentaviary@gmail.com>
2022-08-08 10:16:49 -0600

    feat: Market #vendors_that_sell complete, IT2 complete

--- lib/market.rb
+++ lib/market.rb
@@ -12,5 +12,12 @@ class Market
   def add_vendor(vendor)
     @vendors << vendor
   end
-  
+
+  def vendor_names
+    @vendors.map(&:name)
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all { |vendor| vendor.inventory.map { |stock_item| stock_item[0].name }.include?(item) }
+  end
 end
